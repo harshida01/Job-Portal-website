34874d67c61d1081274a56269d24e882f4c90d88
harshida01 <150689025+harshida01@users.noreply.github.com>
2023-11-17 20:53:31 +0530

    service.php

---
+++ signup11/service.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Services Offered</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Services Offered</h1>
+    </header>
+    <section id="introduction">
+        <h2>Welcome to our job portal!</h2>
+        <p>We provide a range of services to help job seekers and employers connect, making the job search process easier and more efficient.</p>
+    </section>
+    <section id="services">
+        <h2>Our Services:</h2>
+        <ol>
+            <li>Job Search</li>
+            <li>Resume Building</li>
+            <li>Company Profiles</li>
+            <li>Job Alerts</li>
+            <li>Interview Tips</li>
+            <li>Career Resources</li>
+            <li>Employer Services</li>
+        </ol>
+    </section>
+    <section id="how-we-can-help">
+        <h2>How We Can Help:</h2>
+        <p>Whether you're a job seeker looking for your dream job or an employer seeking top talent, we have the tools and services to make the process smoother and more efficient.</p>
+    </section>
+    <section id="contact">
+        <h2>Contact Us:</h2>
+        <p>If you have any questions about our services, feel free to <a href="contact.html">contact us</a>!</p>
+    </section>
+</body>
+<style>
+    body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #007BFF;
+    color: #FFF;
+    text-align: center;
+    padding: 20px;
+}
+
+h1 {
+    margin: 0;
+}
+
+section {
+    margin: 20px;
+}
+
+ol {
+    list-style-type: decimal;
+}
+
+a {
+    color: #007BFF;
+}
+
+/* Add more CSS for styling as needed */
+
+
+
+</style>
+</html>
