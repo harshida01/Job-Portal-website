22e849e0f26ecf47b96f463b35131af93602fffc
harshida01 <150689025+harshida01@users.noreply.github.com>
2023-11-17 20:53:03 +0530

    jobs.php

---
+++ signup11/jobs.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job Search</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        header {
+            background-color: #0073e6;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+
+        h1 {
+            margin: 0;
+        }
+
+        .container {
+            margin: 20px;
+        }
+
+        .job-listing {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 20px;
+        }
+
+        .job-title {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .company-name {
+            color: #0073e6;
+            font-weight: bold;
+        }
+
+        .experience {
+            margin-top: 10px;
+        }
+
+        .package {
+            margin-top: 10px;
+        }
+    
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Job Search</h1>
+    </header>
+    <div class="container">
+        <div class="job-listing">
+            <div class="job-title">Software Developer</div>
+            <div class="company-name">googleapis</div>
+            <div class="experience">Experience: 2-4 years</div>
+            <div class="package">Salary: $70,000 - $90,000 per year</div>
+        </div>
+
+        <div class="job-listing">
+            <div class="job-title">Data Analyst</div>
+            <div class="company-name">Amazon</div>
+            <div class="experience">Experience: 1-3 years</div>
+            <div class="package">Salary: $60,000 - $80,000 per year</div>
+        </div>
+
+        <!-- Four new job listings -->
+        <div class="job-listing">
+            <div class="job-title">Web Designer</div>
+            <div class="company-name">Wipro</div>
+            <div class="experience">Experience: 3-5 years</div>
+            <div class="package">Salary: $55,000 - $75,000 per year</div>
+        </div>
+
+        <div class="job-listing">
+            <div class="job-title">Marketing Manager</div>
+            <div class="company-name">Global Marketing Inc.</div>
+            <div class="experience">Experience: 5-7 years</div>
+            <div class="package">Salary: $80,000 - $100,000 per year</div>
+        </div>
+
+        <div class="job-listing">
+            <div class="job-title">Sales Representative</div>
+            <div class="company-name">Cognizent</div>
+            <div class="experience">Experience: 1-2 years</div>
+            <div class="package">Salary: $40,000 - $60,000 per year</div>
+        </div>
+
+        <div class="job-listing">
+            <div class="job-title">UX/UI Designer</div>
+            <div class="company-name">TCS</div>
+            <div class="experience">Experience: 2-4 years</div>
+            <div class="package">Salary: $60,000 - $80,000 per year</div>
+        </div>
+    </div>
+</body>
+</html>
