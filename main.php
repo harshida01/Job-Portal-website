d62956c499faa18611388f2e95d8086ebfb957b6
harshida01 <150689025+harshida01@users.noreply.github.com>
2023-11-17 20:49:50 +0530

    main.php

---
+++ main.php
@@ -0,0 +1,459 @@
+<!-----META SECTION----->
+<!DOCTYPE html>
+ <html lang="en">
+  <head>
+   <title>Search Job</title>
+
+<style>
+body{
+background-image:url("https://i0.wp.com/mindodyssey.biz/wp-content/uploads/2018/11/pexels-thirdman-7653984.jpg?resize=1170%2C578&ssl=1");
+background-repeat: no-repeat;
+background-size: cover;
+    background-attachment: fixed;
+
+
+}
+</style>
+
+  </head>
+  <body>
+<style>
+* {
+	margin: 0px;
+	padding: 0px;
+}
+html, body {
+	font-family :'Roboto', sans-serif, arial;
+	font-size: 15px;
+
+}
+.container {
+	width: 1170px;
+	margin: 0px auto;
+}
+.width-100 {
+	width: 100%;
+	float: left;
+}
+.width-66 {
+	width: 66%;
+	float: left;
+}
+.width-50 {
+	float: left;
+	width: 50%;
+}
+.width-75 {
+	width: 75%;
+	float: left;
+}
+.width-33 {
+	width: 33%;
+	float: left;
+}
+.width-25 {
+	width: 25%;
+	float: left;
+}
+</style>
+<!------HEADER SECTION HEADER SECTION HEADER SECTION ------>
+<header>
+
+ <div class="width-100">
+    <div class="container header">
+      <div class="width-25">
+ 
+      </div>
+      <div class="width-50">
+        <ul class="header-menu ">
+          <li>
+            <a href="jobs.php">Home </a>
+          </li>
+          <li>
+            <a href="findjobs.php">Jobs </a>
+          </li>
+          <li>
+            <a href="applyjobs.php">Companies </a>
+          </li>
+          <li>
+            <a href="service.php">Services </a>
+          </li>
+          <li>
+            <a href="contact.php">Contact Us </a>
+          </li>
+        </ul>
+      </div>
+      <div class="width-25">
+        <a href="index.php" class="button-job-post">
+          <i class="fa fa-shopping-bag" aria-hidden="true"></i>Login here </a>
+        <a href="signup11.php" class="button-resume">
+          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>Signup here </a>
+      </div>
+    </div>
+  </div>
+</header>
+<style>
+/**HEADER SECTION**/
+.header {
+	padding-top: 20px;
+	padding-bottom: 60px
+}
+.header-menu {
+	list-style: none;
+	margin-top: 10px;
+	
+}
+.header-menu li {
+	float: left;
+	font-size: 19px;
+	
+}
+.header-menu a {
+	font-size: 17px;
+	text-decoration: none;
+	color: white;
+	font-weight: 600;
+    font-size: 20px;
+	padding: 16px;
+	
+}
+.button-job-post {
+	background-color: white;
+	color: black;
+	border: none;
+	border-radius: 2px;
+	font-size: 20px;
+	float: left;
+	padding: 10px;
+	text-decoration: none;
+	
+}
+.button-resume {
+	background-color: white;
+	color: black;
+	border: none;
+	border-radius: 2px;
+	font-size: 20px;
+	float: left;
+	margin-left: 10px;
+	padding: 10px;
+	text-decoration: none;
+	text-align: center;
+}
+</style>
+<!------SEARCH SECTION SEARCH SECTION SEARCH SECTION ------>
+<div class="width-100 banner-section">
+  <div class="container">
+    <br>
+    <br>
+    <br>
+    <h1 class="banner-heading">The Best Job For Your Future</h1>
+    
+  </div>
+</div>
+<style>
+/**BANNER SECTION**/
+.banner-section {
+	background-image: url("../images/banner.jpg");
+	background-repeat: no-repeat;
+	background-size: cover;
+	text-align: center;
+	height: 800px;
+	position: relative;
+	width: 100%;
+}
+.banner-heading {
+	font-size: 60px;
+	color: white;
+	padding-top: 80px;
+	text-align: center;
+	
+}
+
+</style>
+
+<style>
+/**SKILL SECTION SKILL SECTION SKILL SECTION**/
+.skill-section {
+	background-color: #00000008;
+	padding-top: 40px;
+	padding-bottom: 40px;
+}
+.skill-list {
+	background-color: white;
+	width: 23%;
+	float: left;
+	margin-left: 15px;
+	padding-top: 10px;
+	border-radius: 5px;
+	padding-left: 8px;
+	padding-bottom: 10px;
+}
+.skill-list img {
+	width: 70px;
+	height: 70px;
+	border-radius: 100px;
+}
+.skill-list h4 {
+	font-size: 16px;
+	color: #313538;
+	font-weight: bold;
+	padding-left: 8px;
+	padding-top: 8px
+}
+.view-button {
+	width: 45%;
+	height: 20px;
+	border-radius: 20px;
+	border: none;
+	background-color: #e34216;
+	color: white;
+	font-size: 11px;
+	padding-left: 5px;
+	margin-top: 15px;
+}
+</style>
+<!-----RECENT JOB SECTION RECENT JOB SECTION RECENT JOB SECTION ----->
+<div class="width-100 recent-job">
+  <div class="container">
+    <h2 class="recent-job-heading">RECENT JOB</h2>
+    <p class="recent-job-sub-heading">Get the list of all recent jobs</p>
+    <!-----JOB LIST----->
+    <div class="width-50">
+      <div class="recent-job-list">
+        <div class="width-100">
+          <h4 class="job-title">HTML Designer</h4>
+          <p class="job-company">ANASOL Software Solution <i class="fa fa-star" aria-hidden="true"></i> | 234 Reviews </p>
+        </div>
+        <div class="width-33">
+          <i class="fa fa-briefcase" aria-hidden="true"></i> 1 to 4 Years
+        </div>
+        <div class="width-33">
+          <i class="fa fa-map-marker" aria-hidden="true"></i> Hyderabad
+        </div>
+        <div class="width-100">
+          <p class="job-skill">
+            <b>Skills : </b>HTML, CSS, JAVASCRIPT
+          </p>
+        </div>
+        <div class="width-100">
+          <a href="Apply.php" class="job-apply-button">Apply Now</a>
+        </div>
+      </div>
+    </div>
+    <!-----JOB LIST------>
+    <div class="width-50">
+      <div class="recent-job-list">
+        <div class="width-100">
+          <h4 class="job-title">CSS Designer</h4>
+          <p class="job-company">Anasol Group <i class="fa fa-star" aria-hidden="true"></i> 500 Reviews </p>
+        </div>
+        <div class="width-33">
+          <i class="fa fa-briefcase" aria-hidden="true"></i> 1 to 4 Years
+        </div>
+        <div class="width-33">
+          <i class="fa fa-map-marker" aria-hidden="true"></i> Banglore
+        </div>
+        <div class="width-100">
+          <p class="job-skill">
+            <b>Skills : </b> JQUERY, BOOTSTRAP, PHOTOSHOP
+          </p>
+        </div>
+        <div class="width-100">
+          <a href="Apply.php" class="job-apply-button">Apply Now</a>
+        </div>
+      </div>
+    </div>
+    
+<style>
+/**RECENT JOB SECTION RECENT JOB SECTION**/
+.recent-job {
+	padding-top: 50px;
+	background-color: #f0f3fa;
+	padding-bottom: 60px;
+}
+
+.recent-job-heading {
+	text-align: center;
+	color: #343a40;
+}
+
+.recent-job-sub-heading {
+	text-align: center;
+	margin-bottom: 30px;
+}
+
+.recent-job-list {
+	float: left;
+	background-color: white;
+	padding: 20px;
+	margin-left: 10px;
+	margin-right: 10px;
+	margin-bottom: 20px;
+}
+
+.job-title {
+	font-size: 20px;
+}
+
+.job-company {
+	margin-top: 10px;
+	margin-bottom: 20px;
+}
+
+.job-company i {
+	color: orange;
+	margin-left: 10px;
+}
+
+.job-skill {
+	margin-top: 20px;
+	margin-bottom: 10px;
+	font-size: 16px;
+}
+
+.job-apply-button {
+	background: blue;
+	padding: 10px;
+	color: white;
+	text-decoration: none;
+	float: right;
+}
+</style>
+
+
+
+</style>
+<!-------FEATURE SECTION FEATURE SECTION FEATURE SECTION------->
+<div class="width-100 feature-sect">
+  <div class="container">
+    <div class="width-50">
+      <div class="feature-panel">
+        <h4 class="feature-title">DO YOU WANT TO FIND A JOB ?</h4>
+        <button class="feature-btn"
+		onclick="window.location.href='findjobs.php'">Find Job</button>
+      </div>
+    </div>
+    <div class="width-50">
+      <div class="feature-panel">
+        <h4 class="feature-title"> ARE YOU LOOKING FOR A CANDIDATE ?</h4>
+        <button class="feature-btn">Find Candidates</button>
+      </div>
+    </div>
+  </div>
+</div>
+<style>
+/***FEATURE SECTION***/
+.feature-sect {
+	padding-top: 60px;
+	padding-bottom: 60px;
+}
+
+.feature-panel {
+	background-color: #f9f9f9;
+	text-align: center;
+	padding: 30px;
+	border: 1px solid #f0f3fa;
+	margin-left: 20px;
+	margin-right: 20px;
+}
+
+.feature-title {
+	padding-top: 20px
+}
+
+.feature-desc {
+	margin-top: 10px;
+}
+
+.feature-btn {
+	background-color: #1650e2;
+	height: 35px;
+	border: none;
+	color: white;
+	border-radius: 4px;
+	font-size: 13px;
+	margin-top: 10px;
+	padding: 10px;
+}</style>
+<!------FOOTER SECTION FOOTER SECTION FOOTER SECTION ------->
+<footer>
+  <div class="width-100 footer-background">
+    <div class="container">
+      <div class="width-25">
+        <h4>QUICK LINKS</h4>
+        <ul class="footer-list">
+          <li>C / C++ Jobs </li>
+          <li>Java Jobs </li>
+          <li>HTML Jobs </li>
+          <li>PHP Jobs </li>
+        </ul>
+      </div>
+      <div class="width-25">
+        <h4>RESOURCES</h4>
+        <ul class="footer-list">
+          <li>Home</li>
+          <li>Post Free Job</li>
+          <li>Recruiter Login</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div class="width-25">
+        <h4>GET IN TOUCH</h4>
+        <ul class="get-in-touch">
+          <li>
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            <p>E-MAIL: info@mbhg.com</p>
+          </li>
+          <li>
+            <i class="fa fa-headphones" aria-hidden="true"></i>
+            <p>WHATS-APP: +91 -9391689197</p>
+          </li>
+          <li>
+            <i class="fa fa-globe" aria-hidden="true"></i>
+            <p>WEBSITE: https://www.mbhg.com</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<!------FOOTER SECTION FOOTER SECTION FOOTER SECTION ------>
+<style>
+/***FOOTER SECTION***/
+.footer-list {
+	list-style: none;
+	line-height: 35px;
+	padding-top: 20px;
+}
+
+.footer-background {
+	background-color: #222328;
+	padding-top: 60px;
+	padding-bottom: 80px;
+	color: rgba(255, 255, 255, .86);
+}
+
+.get-in-touch {
+	list-style: none;
+	line-height: 15px;
+	padding-top: 20px
+}
+
+.get-in-touch i {
+	float: left;
+	color: #eb372a;
+	font-size: 16px;
+	line-height: 15px;
+}
+
+.get-in-touch p {
+	padding-left: 30px;
+	color: rgba(255, 255, 255, .86);
+	margin-top: 20px
+}
+
+
+</style>
+</body>
+</html>
